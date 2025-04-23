56182afbb306ea04da736721c909eb589a2a2033
leealswl <alswl54321600@gmail.com>
2025-04-24 01:15:47 +0900

    actor no image modify

--- src/pages/MovieDetail/MovieCast.jsx
+++ src/pages/MovieDetail/MovieCast.jsx
@@ -17,16 +17,17 @@ const MovieCast = ({credit}) => {
             xs={6} sm={4} md={3} lg={2}
             className="d-flex justify-content-center"
           >
-            <Card style={{ width: '13rem', height: '23rem' }}>
-              <Card.Img
-                variant="top"
-                src={
-                  member.profile_path
-                    ? `https://media.themoviedb.org/t/p/w276_and_h350_face/${member.profile_path}`
-                    : '/placeholder-profile.png'
-                }
-                alt={member.name}
-              />
+            <Card className="cast-card" style={{ width: '13rem', height: '23rem' }}>
+              {member.profile_path ? (
+                <Card.Img
+                  variant="top"
+                  src={`https://media.themoviedb.org/t/p/w276_and_h350_face/${member.profile_path}`}
+                  alt={member.name}
+                />
+              ) : (
+                <div className="placeholder-img" />
+              )}
+              
               <Card.Body>
                 <Card.Title className="cast-character" style={{ fontWeight: 'bold' }}>
                   {member.character}
