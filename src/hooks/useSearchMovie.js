d47e239cfd95c33dad974fd6e7bf3ac63f7d9511
leealswl <alswl54321600@gmail.com>
2025-04-26 01:55:44 +0900

    search error modify

--- src/hooks/useSearchMovie.js
+++ src/hooks/useSearchMovie.js
@@ -2,40 +2,46 @@ import { useQuery } from "@tanstack/react-query"
 import api from "../utils/api"
 
 
-const fetchSearchMovie=({keyword,page,sortOption,genreFilter })=>{
-  const commonParams = {
-    page,
-    include_adult: false,
-  };
+const fetchSearchMovie = ({ keyword, page, sortOption, genreFilter }) => {
+  const common = { page, include_adult: false };
 
   if (keyword) {
     return api.get("/search/movie", {
-      params: {
-        ...commonParams,
-        query: keyword,
-      },
+      params: { ...common, query: keyword }
     });
   }
 
   return api.get("/discover/movie", {
     params: {
-      ...commonParams,
+      ...common,
       ...(genreFilter ? { with_genres: genreFilter } : {}),
       sort_by: sortOption,
-    },
+    }
   });
 };
 
-export const useSearchMovieQuery=({
-  keyword='',
-  page=1,
+export const useSearchMovieQuery = ({
+  keyword = "",
+  page = 1,
   sortOption = "popularity.desc",
-  genreFilter = '',
-})=>{
-    return useQuery({
-        queryKey:['movie-search',{keyword,page,sortOption,genreFilter }],
-        queryFn:()=>fetchSearchMovie({keyword,page,sortOption,genreFilter }),
-        select:(item)=>item.data,
-        keepPreviousData: true,
-    })
-}
+  genreFilter = "",
+}) => {
+  return useQuery({
+    queryKey: ["movie-search", { keyword, page, sortOption, genreFilter }],
+    queryFn: () =>
+      fetchSearchMovie({ keyword, page, sortOption, genreFilter })
+        .then(res => res.data),
+    keepPreviousData: true,
+    select: data => {
+      if (keyword) {
+        const sorted = [...(data.results || [])].sort((a, b) =>
+          sortOption === "popularity.desc"
+            ? b.popularity - a.popularity
+            : a.popularity - b.popularity
+        );
+        return { ...data, results: sorted };
+      }
+      return data;
+    },
+  });
+};
